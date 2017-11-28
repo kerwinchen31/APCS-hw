f3b7f4ba6353b7b60cac10cf413eba3ab5b353e0
DeBlackBunny <kchen37@stuy.edu>
2017-11-28 02:48:00 -0500

    hw

--- hw_41/SuperArray.java
+++ hw_41/SuperArray.java
@@ -1,9 +1,7 @@
-/***************************
- * class SuperArray
- * Wrapper class for array. Facilitates resizing,
- * getting and setting element values.
- ***************************/
-
+//Kerwin Chen
+//APCS1 pd1
+//HW41 -- Array of Steel
+//2017-11-28
 public class SuperArray
 {
 
@@ -11,31 +9,44 @@ public class SuperArray
   private int _size;    //number of elements in this SuperArray
 
 
-  //default constructor – initializes 10-item array
+  //default constructor - initializes 10-item array
   public SuperArray()
   {
-    /* YOUR IMPLEMENTATION HERE */
+      _data = new int[5];
+      _size = 5;
   }
 
 
   //output SuperArray in [a,b,c] format
   public String toString()
   {
-    /* YOUR IMPLEMENTATION HERE */
+      String ans = "[";
+      for(int x = 0; x < _size; x++){
+	  ans += _data[x];
+	  ans += ", ";
+      }
+      ans = ans.substring(0,ans.length()-2);
+      ans += "]";
+      return ans;
   }
 
 
   //double capacity of SuperArray
   private void expand()
   {
-    /* YOUR IMPLEMENTATION HERE */
+      int[] expanded  = new int[_data.length * 2];
+      for(int x = 0;x<_data.length;x++){
+	  expanded[x] = _data[x];
+      }
+      _data = expanded;
+      _size = _data.length;
   }
 
 
   //accessor -- return value at specified index
   public int get( int index )
   {
-    /* YOUR IMPLEMENTATION HERE */
+      return _data[index];
   }
 
 
@@ -43,14 +54,16 @@ public class SuperArray
   //           return old value at index
   public int set( int index, int newVal )
   {
-    /* YOUR IMPLEMENTATION HERE */
+      int oldVal = _data[index];
+      _data[index] = newVal;
+      return oldVal;
   }
 
 
   //main method for testing
   public static void main( String[] args )
   {
-    /*~~~~~~~~move~me~down~~~~~~~~~~~~~~V~~~~~~~~
+ 
       SuperArray curtis = new SuperArray();
       System.out.println( "Printing empty SuperArray curtis..." );
       System.out.println( curtis );
@@ -67,7 +80,7 @@ public class SuperArray
       System.out.println("Printing expanded SuperArray curtis...");
       System.out.println(curtis);
       }
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~*/
+
   }//end main()
 
 
