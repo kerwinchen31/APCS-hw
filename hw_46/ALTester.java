67c5ea7bcc168b1194fde3828ad05e8b587dc495
DeBlackBunny <kchen37@stuy.edu>
2017-12-05 01:50:56 -0500

    hw

--- hw_46/ALTester.java
+++ hw_46/ALTester.java
@@ -5,6 +5,25 @@
 import java.util.ArrayList;
 
 public class ALTester{
-    int[] a = new int[23];
-    public static void main(String[] args){}
-}
+    static int[] a = new int[23];
+    public static void popArray(int[] b){
+	int length = b.length;
+	for (int x = 0; x<length; x++){
+	    b[x] = (int)(23*Math.random());
+	}
+    }
+    public static boolean sorted(int[] b){
+	int length = b.length;
+	boolean ans = true;
+	for (int x = 0;x<length;x++){
+	    if (!(b[x]>=b[x+1]))
+		ans = false;
+	}
+	return ans;
+	
+    }
+	public static void main(String[] args){
+	    popArray(a);
+	    System.out.println(sorted(a));
+	}
+    }
