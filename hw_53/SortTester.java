45e18d24e80a6adf5816e67f769a0e4471483bc6
Kerwin Chen <kchen37@stuy.edu>
2017-12-14 02:34:49 -0500

    hw

---
+++ hw_53/SortTester.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class SortTester
+{
+      public static ArrayList populate( int size, int lo, int hi ) {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+	    //     offset + rand int on interval [lo,hi]
+	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+	    size--;
+    }
+    return retAL;
+  }
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al ) {
+    int randomIndex;
+    for( int i = al.size()-1; i > 0; i-- ) {
+	    //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+	    //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+    public static void main(String[] args){
+	ArrayList foo = populate(10, 1, 1000);
+	System.out.println(foo);
+	MySorts.bubbleSortV(foo);
+	System.out.println(foo);
+    }
+}
